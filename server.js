28ba55fdc717214cce51a24867bd00b71b144fe8
Lori Timmons <lorietimmons@gmail.com>
2021-11-28 12:32:02 -0600

    query added

--- server.js
+++ server.js
@@ -4,8 +4,9 @@
 // Next, create tables in see 12.1.4
 // Then populate data into the table. See 12.1.5
 
-// Import express
+// Import express / mysql2
 const express = require("express");
+const mysql = require("mysql2");
 
 // Port designation
 const PORT = process.env.PORT || 3001;
@@ -14,6 +15,24 @@ const app = express();
 // middleware - See 12.2.3
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// middleware - Connect to database / mysql
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    // username
+    user: "root",
+    // my SQL P/W
+    password: "Ml512303",
+    database: "election",
+  },
+  console.log("connected to the election database")
+);
+
+// query of the database to test connection 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log('this is the log' ,rows);
+});
 
 // test route / example route - No longer needed.
 // app.get('/', (req, res) => {
