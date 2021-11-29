329f0c75009a62d489a78eea3bc0994b1a688f78
Lori Timmons <lorietimmons@gmail.com>
2021-11-28 23:21:42 -0600

    add CRUD routes

--- server.js
+++ server.js
@@ -8,6 +8,9 @@
 const express = require("express");
 const mysql = require("mysql2");
 
+// import inputCheck
+const inputCheck = require('./utils/inputCheck');
+
 // Port designation
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -30,24 +33,59 @@ const db = mysql.createConnection(
 );
 
 // query of the database to test connection - basically a getAll
-// db.query(`SELECT * FROM candidates`, (err, rows) => {
-//   console.log('this is the log' ,rows);
-// });
-
-// Get one 
-db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
-  if (err) {
-    console.log(err);
+app.get('/api/candidates', (req, res) => {
+  const sql = 'SELECT * FROm candidates';
+db.query(sql, (err, rows) => {
+  if(err) {
+    res.status(500).json({ Error: err.message});
+    return;
   }
-  console.log(row);
+  res.json({
+    message: 'success',
+    data: rows
+    });
+  });
 });
 
-// delete 
-db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log(result);
+// Get a single candidate
+app.get('/api/candidate/:id', (req, res) => {
+  const sql = `SELECT * FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: row
+    });
+  });
+});
+
+// Delete a candidate
+// http method delete() used
+// req.params.id gets specific id
+app.delete('/api/candidate/:id', (req, res) => {
+  const sql = `DELETE FROM candidates WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.statusMessage(400).json({ error: res.message });
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Candidate not found'
+      });
+    } else {
+      res.json({
+        message: 'deleted',
+        changes: result.affectedRows,
+        id: req.params.id
+      });
+    }
+  });
 });
 
 // create 
@@ -62,7 +100,31 @@ db.query(sql, params, (err, result) => {
   console.log(result);
 });
 
+// Create a candidate
+// { body } is destructure to cal req.body
+// inputCheck is validating the entry params 
+// Must import inputCheck
+app.post('/api/candidate', ({ body }, res) => {
+  const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+  VALUES (?,?,?)`;
+const params = [body.first_name, body.last_name, body.industry_connected];
 
+db.query(sql, params, (err, result) => {
+  if (err) {
+    res.status(400).json({ error: err.message });
+    return;
+  }
+  res.json({
+    message: 'success',
+    data: body
+  });
+});
+});
 
 // test route / example route - No longer needed.
 // app.get('/', (req, res) => {
